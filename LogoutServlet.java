de3cb24db7751ebf3fe11adf7ef97d651f859811
JR <180821520+mscbuild@users.noreply.github.com>
2025-05-09 21:26:27 +0300

    Update LogoutServlet.java

--- LogoutServlet.java
+++ LogoutServlet.java
@@ -1,1 +1,23 @@
+package com.system.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false); // prevent creating new session
+        if (session != null) {
+            session.invalidate(); // clear session
+        }
+
+        response.sendRedirect("login.jsp"); // redirect to login
+    }
+}
 
