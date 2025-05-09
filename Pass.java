70d45af18689a20629ca67e01d108e6b80ca156c
JR <180821520+mscbuild@users.noreply.github.com>
2025-05-09 21:17:38 +0300

    Update Pass.java

--- Pass.java
+++ Pass.java
@@ -1,1 +1,6 @@
+import java.security.MessageDigest;
+...
+MessageDigest md = MessageDigest.getInstance("SHA-256");
+byte[] hashed = md.digest(password.getBytes(StandardCharsets.UTF_8));
+String hashedPassword = Base64.getEncoder().encodeToString(hashed);
 
